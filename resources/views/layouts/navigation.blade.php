031373f49bd7d88d731e56ab36b1643d21516cac
SiMENhol <simenhol@uia.no>
2024-11-08 12:32:30 +0100

    Fix for always needing to be logged in

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,30 +18,42 @@
                     <x-nav-link :href="route('booking.index')" :active="request()->routeIs('booking.index')">
                         {{ __('Booking') }}
                     </x-nav-link>
-                    @if(Auth::user()->isAdmin == 1)
-                    <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                    @if (Auth::check())
+                        @if (Auth::user()->isAdmin == 1)
+                        <x-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
                         {{ __('Admin dashboard') }}
-                    </x-nav-link>
+                        </x-nav-link>
+                    @endif
+
                 @endif
+
                 </div>
             </div>
+
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
+                        @if (Auth::check())
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
 
+                            <div>{{ Auth::user()->name }}</div>
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
+                        @else
+                        <div><a href="login">Sign in</a></div>
+
+                    @endif
                     </x-slot>
 
+
                     <x-slot name="content">
+                        @if (Auth::check())
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -56,9 +68,13 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+                        @endif
                     </x-slot>
+
                 </x-dropdown>
+
             </div>
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -83,10 +99,11 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
+                @if (Auth::check()) {
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
-
+            } @endif
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
